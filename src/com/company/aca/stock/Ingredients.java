c07672154724827ba9f76ce86bc062efb8aa5ebf
6gag21 <38590564+6gag21@users.noreply.github.com>
2018-11-17 01:12:07 +0400

    /

--- src/com/company/aca/stock/Ingredients.java
+++ src/com/company/aca/stock/Ingredients.java
@@ -8,6 +8,11 @@ public class Ingredients {
     public static final String INGREDIENT_WATER = "Water";
     public static final String INGREDIENT_SUGAR = "Sugar";
     public static final String INGREDIENT_CREAM = "Cream";
+    private static final int AMOUNT_OF_COFFEE = 1000;
+    private static final int AMOUNT_OF_MILK = 2000;
+    private static final int AMOUNT_OF_CREAM = 1000;
+    private static final int AMOUNT_OF_WATER = 5000;
+    private static final int AMOUNT_OF_SUGAR = 1000;
 
     private static Ingredients instance;
 
@@ -22,11 +27,11 @@ public class Ingredients {
     }
 
     private HashMap<String, Integer> ingredients = new HashMap<>() {{
-        put(INGREDIENT_COFFEE, 1000);
-        put(INGREDIENT_MILK, 2000);
-        put(INGREDIENT_CREAM, 1000);
-        put(INGREDIENT_WATER, 5000);
-        put(INGREDIENT_SUGAR, 1000);
+        put(INGREDIENT_COFFEE, AMOUNT_OF_COFFEE);
+        put(INGREDIENT_MILK, AMOUNT_OF_MILK);
+        put(INGREDIENT_CREAM, AMOUNT_OF_CREAM);
+        put(INGREDIENT_WATER, AMOUNT_OF_WATER);
+        put(INGREDIENT_SUGAR, AMOUNT_OF_SUGAR);
     }};
 
     HashMap<String, Integer> getIngredient() {
